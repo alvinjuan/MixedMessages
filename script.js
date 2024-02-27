d98cb52cd4e0934820f5bf4a9ffbdc131a17440b
Alvin <alvinjasperj@gmail.com>
2024-02-27 00:24:49 -0800

    need to add loops

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 //random number generator
 function randomNumGenerator (num) {
-    Math.floor(Math.random() * num);
+    return Math.floor(Math.random() * num);
 }
 
 // object of an array of random messages
-const randomMessage = {
+const messages = {
     greeting: ['Hello!', "Good Morning!", "How's your day?", "Goodnight!", "How are you?"],
     reminder: ["Wake up at 8 am today", "Don't forget your food", "Study tonight", "Brush your teeth later", "Drink water"],
     motivation: ["Never stop working", "Keep believing", "It's not done till it's done", "Don't lose hope", "Don't doubt yourself"]
@@ -14,6 +14,13 @@ const randomMessage = {
 let messageArr = [];
 
 // access message and push to empty array
-function messageGenerator(){
-    
+
+for (let key in messages){
+    // index is random because of the randomNumGenerator
+    // length is used as the number maximzer for random and then it picks a number between 0 to num
+    let index = randomNumGenerator(messages[key].length);
+    switch(key){
+        case "greeting":
+            console.log()
+    }
 }
