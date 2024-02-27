a02e44c45d245dcc78c095a8a4527506afca863f
Alvin <alvinjasperj@gmail.com>
2024-02-27 00:12:00 -0800

    update

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-// greetings message, reminder, uplifting messages
-function randomNumGenerator () {
-    Math.floor(Math.random() * 5);
+//random number generator
+function randomNumGenerator (num) {
+    Math.floor(Math.random() * num);
 }
 
-console.log("confirmed node");
+// object of an array of random messages
+const randomMessage = {
+    greeting: ['Hello!', "Good Morning!", "How's your day?", "Goodnight!", "How are you?"],
+    reminder: ["Wake up at 8 am today", "Don't forget your food", "Study tonight", "Brush your teeth later", "Drink water"],
+    motivation: ["Never stop working", "Keep believing", "It's not done till it's done", "Don't lose hope", "Don't doubt yourself"]
+}
+
+//empty array to store messages
+let messageArr = [];
+
+// access message and push to empty array
+function messageGenerator(){
+    
+}
